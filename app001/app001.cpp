890d9bd611bb3dfa3b4d6680d55097cb83e100f9
omd24 <omid.miresmaeili@gmail.com>
2022-04-17 09:12:41 +0200

    #ADD improved init/shutdown code

--- app001/app001.cpp
+++ app001/app001.cpp
@@ -4,15 +4,26 @@
 #include <memory.h>
 #include <assert.h>
 
+#include <deque>
+typedef uint32_t LiveIndex;
+typedef std::deque<uint32_t> Uint32Deque;
+
 struct DataBase
 {
-public:
-  DataBase() : m_Capacity(0u), m_RecordSize(0u)
+  DataBase()
+      : m_Capacity(0u),
+        m_RecordSize(0u),
+        m_DataMem(nullptr),
+        m_LiveIndex(nullptr)
   {
   }
   virtual void init(uint32_t p_Capacity) = 0;
+  virtual void shutdown() = 0;
   uint32_t m_Capacity;
   uint32_t m_RecordSize;
+  uint8_t* m_DataMem;
+
+  LiveIndex* m_LiveIndex;
 };
 
 struct StudentData : DataBase
@@ -26,29 +37,96 @@ struct StudentData : DataBase
     m_RecordSize = sizeof(float) * sizeof(uint32_t);
 
     uint32_t memSize = m_RecordSize * m_Capacity;
-    uint8_t* dataMem = (uint8_t*)::malloc(memSize);
-    uint8_t* dataMemCursor = dataMem;
+    m_DataMem = (uint8_t*)::malloc(memSize);
+    uint8_t* dataMemCursor = m_DataMem;
 
-    ::memset(dataMem, 0, memSize);
+    ::memset(m_DataMem, 0, memSize);
 
+    m_LiveIndex = (LiveIndex*)dataMemCursor;
+    dataMemCursor += m_Capacity * sizeof(LiveIndex);
     m_Score = (float*)dataMemCursor;
     dataMemCursor += m_Capacity * sizeof(float);
     m_Age = (uint32_t*)dataMemCursor;
     dataMemCursor += m_Capacity * sizeof(uint32_t);
+  }
+
+  void shutdown() override
+  {
+    m_Age = nullptr;
+    m_Score = nullptr;
+    m_LiveIndex = 0;
+    m_RecordSize = 0;
+    m_Capacity = 0;
+
+    ::free(m_DataMem);
+    m_DataMem = nullptr;
   }
 };
 
 template <typename T> struct ObjectManagerBase
 {
   static T ms_Data;
+  static uint8_t* ms_LiveObjects;
+  static uint32_t ms_LiveObjectCount;
+  static uint32_t ms_LiveObjectCountMax;
+  static Uint32Deque* ms_FreeIndices;
+  static uint32_t ms_GenerationCount;
+
+  /// The number of objects freed before reusing index slots
+  static uint32_t OBJECT_MINIMUM_FREE_INDICES;
 };
+
+/// Static data definitions
 template <typename T> T ObjectManagerBase<T>::ms_Data;
+
+template <typename T> uint8_t* ObjectManagerBase<T>::ms_LiveObjects;
+
+template <typename T> uint32_t ObjectManagerBase<T>::ms_LiveObjectCount;
+
+template <typename T> uint32_t ObjectManagerBase<T>::ms_LiveObjectCountMax;
+
+template <typename T>
+std::deque<uint32_t>* ObjectManagerBase<T>::ms_FreeIndices = nullptr;
+
+template <typename T> uint32_t ObjectManagerBase<T>::ms_GenerationCount;
+
+template <typename T>
+uint32_t ObjectManagerBase<T>::OBJECT_MINIMUM_FREE_INDICES = 1024;
 
 struct StudentManager : ObjectManagerBase<StudentData>
 {
   static void init(uint32_t p_Capacity)
   {
+    _initializeDataBase(p_Capacity);
+
+    // other specific initialization code
+    //
+  }
+  static void shutdown()
+  {
+    _deallocateDataBase();
+  }
+
+  static void _initializeDataBase(uint32_t p_Capacity)
+  {
     ms_Data.init(p_Capacity);
+    ms_LiveObjectCount = 0;
+    ms_LiveObjectCountMax = 0;
+    ms_LiveObjects = (uint8_t*)::malloc(p_Capacity * sizeof(uint8_t));
+    memset(ms_LiveObjects, 0xFFFFFFFF, p_Capacity * sizeof(uint8_t));
+
+    ms_FreeIndices = new Uint32Deque();
+  }
+  static void _deallocateDataBase()
+  {
+    ms_Data.shutdown();
+
+    delete ms_FreeIndices;
+
+    ms_LiveObjects = nullptr;
+    ms_FreeIndices = nullptr;
+    ms_LiveObjectCount = 0u;
+    ms_GenerationCount = 0u;
   }
 };
 
@@ -69,5 +147,6 @@ int main()
     studentMgr.ms_Data.m_Score[i] = 20.0f;
   }
 
+  studentMgr.shutdown();
   return (0);
 }
